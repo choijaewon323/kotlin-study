866a913d7ac29b212adb55443be00d044c4ecb83
choijaewon323 <casdfghjke@naver.com>
2026-05-15 21:31:21 +0900

    first commit

---
+++ src/main/kotlin/com/kotlin/study/controller/UserController.kt
@@ -0,0 +1,12 @@
+package com.kotlin.study.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/user")
+class UserController {
+    @GetMapping("/hello")
+    fun hello() = "hello world"
+}
